4807cd17f9057cd557a93db5e596eb68a2b2385c
Lin Yun Wen <l40303k@gmail.com>
2021-05-31 07:53:43 +0800

    Feature: add redux

---
+++ src/reducers/record.js
@@ -0,0 +1,16 @@
+export function recordReducer(state = {}, action) {
+    switch (action.type) {
+        case "SET_MODAL": {
+            break;
+        }
+        case "SET_CONTEXT_MENU": {
+            break;
+        }
+        case "REFRESH_UI": {
+            break;
+        }
+        default:
+            return state;
+    }
+    return newState;
+}
